d641060845c05f3ef1d54ee906abe4497a8aba2c
Carlos Villamil <c.villamilc@uniandes.edu.co>
2023-03-27 13:40:24 -0500

    fix lint issues

--- src/tests/test_file_handler.py
+++ src/tests/test_file_handler.py
@@ -19,7 +19,7 @@ class TestFileHandler:
         else:
             print("The file does not exist")
 
-    def test_load_data_correct_path(self, file_created, path):        
+    def test_load_data_correct_path(self, file_created, path):
         file_info = load_data(path)
         assert file_info == 'RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00\nASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00'  # noqa: E501
 
@@ -41,11 +41,11 @@ class TestFileHandler:
                              'ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00']  # noqa: E501
 
     def test_input_handler_incorrect_path(self, file_created):
-        dir_name = '/opt'      
+        dir_name = '/opt'
         file_info = input_handler(dir_name)
         assert file_info is None
 
     def test_output_handler_correct_path(self):
-        dir_name = os.path.dirname(__file__)        
+        dir_name = os.path.dirname(__file__)
         file_info = output_handler('RENE:215', dir_name)
         assert file_info == 8
